2cde21c1aa5e9208d390f4db8f997ad3c861b9ed
summeryio <summeryio@summeryiodeiMac.local>
2020-07-07 21:20:56 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -7,25 +7,6 @@ import count from './modules/count'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  /* state: {
-    count: 0
-  },
-  mutations: {
-    increment(state) {
-      state.count ++
-    },
-    decrement(state) {
-      state.count --
-    }
-  },
-  actions: {
-    increment: ({commit}) => {
-      commit('increment')
-    },
-    decrement: ({commit}) => {
-      commit('decrement')
-    }
-  }, */
   modules: {
     money,
     count
